73b54084ca660f6029040729d573ef32c7a74c8b
Tyler Chauhan <tjchauhan7@gmail.com>
2018-08-02 16:00:04 -0400

    Geolocation addition

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -23,11 +23,10 @@ io.on('connection', (socket) => {
         console.log('createMessage:', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This from the server');
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.lat, coords.lon));
     });
 
     socket.on('disconnect', () => {
